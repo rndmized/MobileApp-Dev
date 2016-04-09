06688a8cb8332b8745bcc40f86c6874342679c99
Albert Rando <a.rndmized@gmail.com>
2016-04-09 13:43:38 +0100

    Preliminary Layout Design. Swipe Testing Implemented.

--- todo_project/www/js/controllers.js
+++ todo_project/www/js/controllers.js
@@ -1,28 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('_todoCtrl', function($scope) {
+    
+    $scope.mylist = [1,2,3];
+    
+    $scope.add = function(){
+        
+        $scope.mylist.push($scope.mylist[$scope.mylist.length-1]+1);
+    };
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('CompletedCtrl', function($scope) {
+    
+    
+    
+    $scope.complete = function(){
+        console.log("Completed");
+    }
+    
+     $scope.delete = function(){
+        console.log("Deleted");
+    }
+    
+    
 });
